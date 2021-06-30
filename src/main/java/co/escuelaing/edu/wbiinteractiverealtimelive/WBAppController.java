7c0e53349913b4ee5bd783f0d8047c3c4bb9adfa
andrewcubillos <felipecubillos010@gmail.com>
2021-06-30 00:39:23 -0500

    casi en heroku

--- src/main/java/co/escuelaing/edu/wbiinteractiverealtimelive/WBAppController.java
+++ src/main/java/co/escuelaing/edu/wbiinteractiverealtimelive/WBAppController.java
@@ -5,11 +5,26 @@
  */
 package co.escuelaing.edu.wbiinteractiverealtimelive;
 
+import java.util.Collections;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WBAppController {
- public static void main(String[] args){
- SpringApplication.run(WBAppController.class,args);
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(WBAppController.class);
+
+        app.setDefaultProperties(Collections.singletonMap("server.port", getPort()));
+        app.run(args);
+    }
+
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 8080; 
+    
+
+
  }
 }
